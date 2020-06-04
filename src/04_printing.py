4bd111bd880ce841f5ca1c7f1ec2988e2fd53a09
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-06-03 19:07:14 -0500

    Completed lists.

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,6 +12,8 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+# STILL NEED TO DO THIS
+
 # Use the 'format' string method to print the same thing
 
 print(f"x is {x}, y is {y}, z is {z}")
